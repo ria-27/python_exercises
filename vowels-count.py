e538949a8637927288516a48cc61acb8deb34935
ria-27 <riavasishta@gmail.com>
2026-01-12 19:45:30 +0530

    Add files via upload

---
+++ vowels-count.py
@@ -0,0 +1,15 @@
+# Program to write a python function that takes a string input from the user and counts the number of vowels and consonants in the string
+def vowel_count(text):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    vowels_count = 0
+    consonant_count = 0
+    for char in text:
+        if char.isalpha():           #ignoring space,number,punctuation and checks if letter
+            if char in vowels:
+                vowels_count += 1
+            else:
+                consonant_count += 1
+
+    print("Vowel count=", vowels_count)
+    print("Consonant count=", consonant_count)
+vowel_count(input("Enter a text: "))
