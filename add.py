c2daca9fb7f0a36f5c5c032841929c1f0a9e18cb
ria-27 <riavasishta@gmail.com>
2026-01-21 19:48:58 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,8 @@
+# Program to write a python function to add two elements and display the result. The element can be of type integer, float or string.
+def add_elements(a,b):
+    result= a+b
+    print("Result:",result)
+add_elements(5,10)
+add_elements(3.5,2.5)
+add_elements("hello ","world")
+
