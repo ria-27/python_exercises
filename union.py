946767c2b12bd1f5e6af89b7adfeaa1603e7f193
ria-27 <riavasishta@gmail.com>
2026-01-21 19:55:39 +0530

    Add files via upload

---
+++ union.py
@@ -0,0 +1,9 @@
+# Write a python code block to input numbers into two sets.
+# Perform union, intersection, difference operations on the sets and print the results/
+s1={4,4,56,7}
+s2={45,4,7,3}
+print(s1.union(s2))
+print(s1.intersection(s2))
+print(s1.difference(s2))
+
+
